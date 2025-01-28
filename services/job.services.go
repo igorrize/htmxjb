63475f8b3fc53f7395c89b77c1db3855ff75c7d8
igorrize <igor.lobazoff@gmail.com>
2025-01-28 23:16:06 +0200

    f* tailwind we used cdn

--- services/job.services.go
+++ services/job.services.go
@@ -7,10 +7,16 @@ import (
 )
 
 type Job struct {
-	ID          int       `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description,omitempty"`
-	CreatedAt   time.Time `json:"created_at,omitempty"`
+    ID          int       `json:"id"`
+    Title       string    `json:"title"`
+    Description string    `json:"description,omitempty"`
+    CreatedAt   time.Time `json:"created_at,omitempty"`
+    Type        string    `json:"type"`          
+    Location    string    `json:"location"`       
+    Salary      string    `json:"salary"`         
+    IsNew       bool      `json:"is_new"`         
+    Company     string    `json:"company"`        
+    Tags        []string  `json:"tags,omitempty"` 
 }
 
 type JobServices struct {
--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["../views/**/*.{templ,go}"],
   theme: {
     extend: {},
   },
